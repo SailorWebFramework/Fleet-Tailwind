c4b72a9452ccfdffad1dca4416ff4a46fcd633cc
Joshua Davis <chess.ofx@gmail.com>
2024-04-24 19:17:38 -0700

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 5.9
+import PackageDescription
+import CompilerPluginSupport
+
+let package = Package(
+    name: "Tailwind",
+    platforms: [
+        .macOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "Tailwind",
+            targets: ["Tailwind"]
+      )
+    ],
+    dependencies: [ ],
+    targets: [
+        .target(
+            name: "Tailwind",
+            dependencies: [],
+            path: "Sailboat"
+        )
+    ]
+)
